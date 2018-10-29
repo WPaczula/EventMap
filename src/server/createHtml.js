8ada4431d5c2e79c41fa491fbff2018f589d3615
WojtekP <wojciech.paczula@gmail.com>
2018-10-29 21:44:09 +0100

    Add styles to SSR application

--- src/server/createHtml.js
+++ src/server/createHtml.js
@@ -1,10 +1,15 @@
 import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
 import App from '../app'
 import withTemplate from './withTemplate'
 
 const createHtml = () => {
-  const html = renderToString(App)
-  return withTemplate(html)
+  const sheet = new ServerStyleSheet()
+
+  const html = renderToString(sheet.collectStyles(App))
+  const styles = sheet.getStyleTags()
+
+  return withTemplate(html, styles)
 }
 
 export default createHtml
--- src/server/withTemplate.js
+++ src/server/withTemplate.js
@@ -1,8 +1,9 @@
-const withTemplate = html => `
+const withTemplate = (html, styles) => `
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
+    ${styles}
 </head>
 
 <body>
